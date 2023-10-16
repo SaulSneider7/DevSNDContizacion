6fe944b6785673607ecdb783a250037a074d686b
saulsneider7 <saulsneider@hotmail.com>
2023-10-16 00:37:12 -0500

    mensaje consola

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+console.log('%c¡Atención! %c', 'font-size: 24px; color: yellow; background-color: red;', 'font-size: 20px;');
+console.log('%cEsta es una zona de desarrollo. No puedes ingresar a este apartado del sitio web.', 'font-size: 20px;');
+
 let formulario = document.getElementById('form');
 formulario.addEventListener('submit', (e) => {
     Swal.fire({
