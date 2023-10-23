1bd689845dc2c02968ca0f42894b5ab55eb5e4c0
saulsneider7 <saulsneider@hotmail.com>
2023-10-23 00:30:24 -0500

    email js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,31 @@ console.log('%cEsta es una zona de desarrollo. No puedes ingresar a este apartad
 
 let formulario = document.getElementById('form');
 formulario.addEventListener('submit', (e) => {
-    Swal.fire({
-        title: "Formulario Enviado",
-        text: "El formulario se ha enviado correctamente.",
-        icon: "success",
-    })
+    
 })
+
+// EMAIL JS
+
+const btn = document.getElementById('cotizar');
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.innerHTML = `<i class="fa-solid fa-paper-plane"></i>&nbsp;&nbsp; Cotizando ...`;
+
+        const serviceID = 'default_service';
+        const templateID = 'template_tb5qpw7';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.innerHTML = `<i class="fa-solid fa-paper-plane"></i>&nbsp;&nbsp; Cotizar Sitio Web`;
+                Swal.fire({
+                    title: "Formulario Enviado",
+                    text: "El formulario se ha enviado correctamente.",
+                    icon: "success",
+                })
+            }, (err) => {
+                alert(JSON.stringify(err));
+            });
+    });
